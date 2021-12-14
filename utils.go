b253ff213abfd4d6c2c06aacd5022f41566efd92
gueencode <navarro.alexis98@gmail.com>
2021-12-14 11:50:20 -0700

    Update utils.go

--- utils.go
+++ utils.go
@@ -1,14 +1,15 @@
 package grmm
 
 import (
+	"errors"
 	"os"
 )
 
 func FileExists(path string) bool {
-	if _, err := os.Stat(path); err != nil {
-		if os.IsNotExist(err) {
+	if _, err := os.Stat(path); err == nil {
+			return true
+		} else if errors.Is(err, os.ErrNotExist) {
 			return false
 		}
 	}
-	return true
 }
