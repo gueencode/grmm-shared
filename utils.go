9b4e976937169e9bf46ed572f4575fc383de2249
gueencode <navarro.alexis98@gmail.com>
2021-12-15 01:18:52 -0700

    Update utils.go

--- utils.go
+++ utils.go
@@ -7,9 +7,9 @@ import (
 
 func FileExists(path string) bool {
 	if _, err := os.Stat(path); err == nil {
-			return true
-		} else if errors.Is(err, os.ErrNotExist) {
-			return false
-		}
+		return true
+	} else if errors.Is(err, os.ErrNotExist) {
+		return false
 	}
+	return false
 }
